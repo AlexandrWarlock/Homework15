4212101d9fec9a5bb9321485187cd00a21439e57
AlexandrWarlock <alexandrpetrov291094@gmail.com>
2023-08-07 14:20:17 +0300

    Домашнее задание 15

---
+++ src/main/java/com/alexandrwarlock/calculator/CalculatorApplication.java
@@ -0,0 +1,14 @@
+package com.alexandrwarlock.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CalculatorApplication.class, args);
+    }
+
+}
